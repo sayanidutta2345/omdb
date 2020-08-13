22bdcd679243eba773172abb4dc655eece11a3ca
sayanidutta2345 <sayanidutta2345@gmail.com>
2020-08-13 23:57:07 +0530

    env variables

--- app.js
+++ app.js
@@ -2,19 +2,24 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	next();
+  });
+
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
+app.use('/omdb', indexRouter);
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 var axios = require('axios');
 
-var OMDB_API_KEY=process.env.OMDB_API_KEY;
-var OMDB_URL=process.env.OMDB_URL;
+var OMDB_API_KEY=process.env.OMDB_API_KEY ;
+var OMDB_URL=process.env.OMDB_URL ;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -14,6 +14,7 @@ router.get('/', function(req, res, next) {
 router.get('/search', (req, res, next) => {
   const title = req.query.title
   const url = `${OMDB_URL}?apikey=${OMDB_API_KEY}&s=${title}`
+  //const url =`http://omdbapi.com/?apikey=17b41a3e&s=${title}`
   axios.get(url)
     .then(response => {
       res.send(response.data);
